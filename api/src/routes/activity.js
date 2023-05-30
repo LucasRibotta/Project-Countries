0281bd2fd3178dd662819a13e1bd5403bad777a0
LucasRibotta <lucasribotta15@gmail.com>
2023-05-29 23:03:16 -0300

    back ok solucion

--- api/src/routes/activity.js
+++ api/src/routes/activity.js
@@ -1,73 +1,38 @@
-const request = require('supertest');
-const app = require('../app');
-const Activity = require('../db/models/activity');
-const Country = require('../db/models/country');
+const { Router } = require('express');
+const { Activity, Country } = require('../db');
+const {getCreateActivity} = require('../controllers/activities')
 
-describe('Activities API', () => {
-  beforeAll(async () => {
-    await Activity.destroy({ truncate: true });
-    await Country.destroy({ truncate: true });
-  });
+const router = Router();
 
-  afterAll(async () => {
-    await Activity.destroy({ truncate: true });
-    await Country.destroy({ truncate: true });
-  });
+router.get('/', async (req, res) => {
+    try {
+        // Buscar todas las actividades en la base de datos
+       const allActivities = await Activity.findAll({
+          include: Country
+       })
+       // Devolvemos las acitividades encontradas como respuesta
+       res.status(200).json(allActivities)
+    } catch (error) {
+       res.status(400).json({ error: "No se encontraron actividades" })
+    }
+ 
+ });
 
-  describe('GET /activities', () => {
-    it('should return all activities', async () => {
-      const activity1 = await Activity.create({ name: 'Activity 1' });
-      const activity2 = await Activity.create({ name: 'Activity 2' });
 
-      const response = await request(app).get('/activities');
+router.post("/", async (req,res,next) => {
+    try {
+         // Crear una nueva actividad utilizando los datos recibidos en el cuerpo de la solicitud
+        const response = await getCreateActivity(req.body);
+        res.status(201).json({
+            status: 'Se ha creado exitosamente'
+        })
+    
+    } catch (error) {
+        
+        res.status(400).json({error: 'error en la creacion'})
+        next(error)
+    }
+})
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual([
-        {
-          id: activity1.id,
-          name: 'Activity 1',
-          country: null,
-        },
-        {
-          id: activity2.id,
-          name: 'Activity 2',
-          country: null,
-        },
-      ]);
-    });
 
-    it('should return an empty array if no activities are found', async () => {
-      const response = await request(app).get('/activities');
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual([]);
-    });
-  });
-
-  describe('POST /activities', () => {
-    it('should create a new activity', async () => {
-      const country = await Country.create({ name: 'Country 1' });
-      const response = await request(app)
-       .post('/activities')
-       .send({ name: 'Activity 1', countryId: country.id });
-
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual({
-        status: 'Se ha creado exitosamente',
-      });
-
-      const activity = await Activity.findOne({ where: { name: 'Activity 1' } });
-      expect(activity).not.toBeNull();
-      expect(activity.countryId).toBe(country.id);
-    });
-
-    it('should return an error if the request body is invalid', async () => {
-      const response = await request(app).post('/activities');
-
-      expect(response.status).toBe(400);
-      expect(response.body).toEqual({
-        error: 'error en la creacion',
-      });
-    });
-  });
-});
+module.exports = router;
