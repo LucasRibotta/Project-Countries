757f82fd8fddcda57ca2f60dc54d1cbb922526a3
LucasRibotta <lucasribotta15@gmail.com>
2023-05-31 01:11:08 -0300

    data

--- api/src/app.js
+++ api/src/app.js
@@ -25,7 +25,7 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pi-countries-gules-iota.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
