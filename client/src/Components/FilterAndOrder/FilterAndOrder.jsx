1a8742c2323c93d12eda761d51491ddac8724f6f
LucasRibotta <lucasribotta15@gmail.com>
2023-06-01 03:09:32 -0300

    prueba

--- client/src/Components/FilterAndOrder/FilterAndOrder.jsx
+++ client/src/Components/FilterAndOrder/FilterAndOrder.jsx
@@ -1,38 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterByContinents, filterBySeason, clearFilter } from '../../Redux/actions/actions';
+import {
+  filterByContinents,
+  filterBySeason,
+  clearFilter,
+} from "../../Redux/actions/actions";
 import FilterActivity from "./FilterActivity";
 import FilterContinents from "./FilterContinents";
 import OrderNameAlpha from "./OrderAlfa";
 import OrderPopulation from "./OrderPopulation";
 import ClearFilter from "./ClearFilter";
 
-
 export default function FilterAndOrder({ setCurrentPage }) {
   const dispatch = useDispatch();
-  const selectedContinent = useSelector(
-    (state) => state.filters.selectedContinent
-  );
-  const selectedSeason = useSelector((state) => state.filters.selectedSeason);
+  const allContinents = useSelector((state) => state.allContinents);
+  const season = useSelector((state) => state.season);
+
+  const [selectedOrder, setSelectedOrder] = useState(""); // Estado para el ordenamiento seleccionado
 
   useEffect(() => {
-    // Realizar el filtrado por continente y temporada al cargar la página
-    if (selectedContinent) {
-      dispatch(filterByContinents(selectedContinent));
+    // Realizar el filtrado por temporada al cargar la página
+    if (season) {
+      dispatch(filterBySeason(season));
     }
-    if (selectedSeason) {
-      dispatch(filterBySeason(selectedSeason));
-    }
-  }, [dispatch, selectedContinent, selectedSeason]);
+  }, [dispatch, season]);
 
   function handleFilterContinents(continent) {
     setCurrentPage(1);
     dispatch(filterByContinents(continent));
+
+    // Si hay una temporada seleccionada, realizar el filtrado por temporada y continente
+    if (season) {
+      dispatch(filterBySeason(season, continent));
+    }
   }
 
   function handleFilterSeason(season) {
     setCurrentPage(1);
     dispatch(filterBySeason(season));
+
+    // Si hay un continente seleccionado, realizar el filtrado por temporada y continente
+    if (allContinents) {
+      dispatch(filterBySeason(season, allContinents));
+    }
+  }
+
+  function handleOrderSelection(order) {
+    setCurrentPage(1);
+    setSelectedOrder(order);
   }
 
   function handleClearFilters() {
@@ -50,8 +65,16 @@ export default function FilterAndOrder({ setCurrentPage }) {
         setCurrentPage={setCurrentPage}
         handleFilterSeason={handleFilterSeason}
       />
-      <OrderNameAlpha setCurrentPage={setCurrentPage} />
-      <OrderPopulation setCurrentPage={setCurrentPage} />
+      <OrderNameAlpha
+        setCurrentPage={setCurrentPage}
+        handleOrderSelection={handleOrderSelection}
+        selected={selectedOrder === "nameAlpha"}
+      />
+      <OrderPopulation
+        setCurrentPage={setCurrentPage}
+        handleOrderSelection={handleOrderSelection}
+        selected={selectedOrder === "population"}
+      />
       <ClearFilter handleClearFilters={handleClearFilters} />
     </div>
   );
