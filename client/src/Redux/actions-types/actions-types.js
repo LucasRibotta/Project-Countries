387268c219c5336492fddcaec2fd54a0f77264f7
LucasRibotta <lucasribotta15@gmail.com>
2023-07-24 18:46:16 -0300

    fix data ok

--- client/src/Redux/actions-types/actions-types.js
+++ client/src/Redux/actions-types/actions-types.js
@@ -1,4 +1,4 @@
-export const url = "https://pi-countries-production-ce75.up.railway.app/"
+export const url = "https://pi-countries-production-ce75.up.railway.app"
 //"http://localhost:3001"
 //"https://pi-countries-production-e7e8.up.railway.app"
 export const GET_COUNTRIES = "GET_COUNTRIES";
